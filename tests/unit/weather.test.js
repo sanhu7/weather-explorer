c8d02589adbde07c133fe6105bea6b43cc35bb39
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-11 11:32:33 +0200

    skriv tester för väder-beskrivning(getWeatherDescription)

--- tests/unit/weather.test.js
+++ tests/unit/weather.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { fahrenheitToCelsius } from '../../src/weather.js'
+import { fahrenheitToCelsius, getWeatherDescription } from '../../src/weather.js'
 
 describe('fahrenheitToCelsius', () => {
     it('should convert 32°F to 0°C', () => {
@@ -14,3 +14,21 @@ describe('fahrenheitToCelsius', () => {
         expect(fahrenheitToCelsius(14)).toBe(-10);
     });
 });
+
+describe("getWeatherDescription", () => {
+    it("should returns "Freezing" for temperatures below 0°C", () => {
+        expect(getWeatherDescription(-5)).toBe("Freezing");
+    })
+
+    it("should returns "Cold" for temperatures between 0°C and 14°C", () => {
+        expect(getWeatherDescription(10)).toBe("Cold");
+    })
+
+    it("should returns "Warm" for temperatures between 15°C and 24°C", () => {
+        expect(getWeatherDescription(20)).toBe("Warm");
+    })
+
+    it("should returns "Hot" for temperatures above 25°C", () => {
+        expect(getWeatherDescription(30)).toBe("Hot");
+    })
+})
