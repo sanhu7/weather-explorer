38382f150bbce15a984d86e2f8f59aaad5968444
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-11 11:45:09 +0200

    städa upp getWeatherDescription för att gör koden enklare att förstå

--- src/weather.js
+++ src/weather.js
@@ -1,13 +1,18 @@
 const FREEZING_POINT_F = 32;
 const CONVERSION_FACTOR = 5 / 9;
+const FREEZING = 0
+const COLD_MAX = 15
+const WARM_MAX = 25
+
+
 
 export function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - FREEZING_POINT_F) * CONVERSION_FACTOR;
 }
 
 export function getWeatherDescription(celsius) {
-    if (celsius < 0) return "Freezing"
-    if (celsius < 15) return "Cold"
-    if (celsius < 25) return "Warm"
+    if (celsius < FREEZING) return "Freezing"
+    if (celsius < COLD_MAX) return "Cold"
+    if (celsius < WARM_MAX) return "Warm"
     return "Hot"
 }
