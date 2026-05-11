8403d115c3ec92f450e62bf2d9402047b69463b2
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-11 11:22:17 +0200

    refactor: gör koden lättare att läsa

--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,6 @@
+const FREEZING_POINT_F = 32;
+const CONVERSION_FACTOR = 5 / 9;
+
 export function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - FREEZING_POINT_F) * CONVERSION_FACTOR;
 }
