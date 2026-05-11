d98f9e8a900fb6a1dfefb3da34ba47138516fc6d
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-11 12:35:47 +0200

    lägg till tester för API-anslutning

---
+++ tests/integration/api.test.js
@@ -0,0 +1,16 @@
+import {describe, it, expect} from 'vitest';
+import { fetchWeatherData, fetchCoordinates } from '../../src/api.js';
+
+describe('API Integration Tests', () => {
+    it('should fetches coordinates for a city', async () => {
+        const results = await fetchCoordinates('Stockholm')
+        expect(results).toHaveProperty('latitude')
+        expect(results).toHaveProperty('longitude')
+    });
+
+    it('should fetches weather data using coordinates', async () => {
+        const results = await fetchWeatherData(59.33, 18.07)
+        expect(results).toHaveProperty('temperature')
+        expect(results).toHaveProperty('windSpeed')
+    });
+});
