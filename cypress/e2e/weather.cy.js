8693b2fcbf117d4bf6e2b0d15d4b06590a2303e0
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-12 13:12:20 +0200

    skriv stad i sökrutan

--- cypress/e2e/weather.cy.js
+++ cypress/e2e/weather.cy.js
@@ -1,7 +1,8 @@
 
 describe('Weather App E2E Tests', () => {
     it('should  fetch and display weather when user search for a city', () => {
-        cy.visit('http://localhost:5173')
+        cy.visit('http://localhost:5173'),
+            cy.get('input[name="city"]').type('Stockholm')
 
     })
 })
