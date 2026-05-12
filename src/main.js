9376d4628ea6c4a1ba20e4e94fd1a62e9185213e
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-12 12:12:00 +0200

    fånga formulärets submit

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ import { fetchCoordinates, fetchWeatherData } from "./api.js";
 import { renderWeatherCard } from "./ui.js";
 
 const form = document.getElementById('weather-form')
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const city = e.target.city.value
+    console.log('Stad:', city)
+})
