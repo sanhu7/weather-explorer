4958535f327d359c87d879264eef87bb219bf348
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-12 13:14:49 +0200

    klicka på search-knappen i E2E-testet

--- cypress/e2e/weather.cy.js
+++ cypress/e2e/weather.cy.js
@@ -2,7 +2,8 @@
 describe('Weather App E2E Tests', () => {
     it('should  fetch and display weather when user search for a city', () => {
         cy.visit('http://localhost:5173'),
-            cy.get('input[name="city"]').type('Stockholm')
+            cy.get('input[name="city"]').type('Stockholm'),
+            cy.get('button[type="submit"]').click()
 
     })
 })
