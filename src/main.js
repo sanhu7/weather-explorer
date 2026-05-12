3487adfe5d71b0d35e1d9354f58469631adeb262
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-12 12:25:23 +0200

    hämta väderdata från koordinater

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ form.addEventListener('submit', async (e) => {
     const city = e.target.city.value
 
     const coords = await fetchCoordinates(city)
-    console.log('Stad:', city)
-    console.log('Coördinaten:', coords)
+    const weather = await fetchWeatherData(coords.latitude, coords.longitude)
+    console.log('Väder:', weather)
 })
