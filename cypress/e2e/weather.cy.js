e09e99e331a282dd958f33367073300c157f1ba9
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-12 13:17:32 +0200

    lägg till kontroll av vädresultat i E2E-testet

--- cypress/e2e/weather.cy.js
+++ cypress/e2e/weather.cy.js
@@ -4,6 +4,10 @@ describe('Weather App E2E Tests', () => {
         cy.visit('http://localhost:5173'),
             cy.get('input[name="city"]').type('Stockholm'),
             cy.get('button[type="submit"]').click()
+        cy.get('#weather-result', { timeout: 10000 }).should('contain', 'Stockholm')
+        cy.get('#weather-result').should('contain', 'Temperature')
+        cy.get('#weather-result').should('contain', 'Wind Speed')
+
 
     })
 })
