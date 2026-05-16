c471acdd659beaff0d520b165c2c8227939dfbe3
Sana Naqeeb <sansoonm2012@gmail.com>
2026-05-16 15:50:56 +0200

    test: improve E2E test with visibility check

--- cypress/e2e/weather.cy.js
+++ cypress/e2e/weather.cy.js
@@ -1,10 +1,14 @@
 
 describe('Weather App E2E Tests', () => {
     it('should  fetch and display weather when user search for a city', () => {
-        cy.visit('http://localhost:5173'),
-            cy.get('input[name="city"]').type('Stockholm'),
-            cy.get('button[type="submit"]').click()
-        cy.get('#weather-result', { timeout: 10000 }).should('contain', 'Stockholm')
+        cy.visit('http://localhost:5173')
+        cy.get('input[name="city"]').type('Stockholm')
+        cy.get('button[type="submit"]').click()
+
+
+        cy.get('#weather-result', { timeout: 10000 })
+            .should('be.visible')
+            .should('contain', 'Stockholm')
         cy.get('#weather-result').should('contain', 'Temperature')
         cy.get('#weather-result').should('contain', 'Wind Speed')
 
